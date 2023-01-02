4d9595b13f685da9b3feec4ea5e7644304a26b5c
Charles Fettinger <charles@onacloud.org>
2023-01-02 11:52:07 -0800

    ug fixes for 1.5

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,8 @@
 set PYTHON=C:\Users\CharlesFettinger\AppData\Local\Programs\Python\Python310\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--gfpgan-model GFPGANv1.4.pth --api --no-progressbar-hiding --xformers --gradio-img2img-tool color-sketch --skip-torch-cuda-test --precision full --no-half
-rem --gfpgan-model GFPGANv1.4.pth
+set COMMANDLINE_ARGS=--gfpgan-model GFPGANv1.4.pth --xformers --no-progressbar-hiding --gradio-img2img-tool color-sketch --skip-torch-cuda-test --disable-safe-unpickle
+rem --xformers --precision full --no-half  --medvram
+set 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512'
+rem set ACCELERATE="True"
 call webui.bat
