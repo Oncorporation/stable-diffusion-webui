3d280b108f15998ada50f6ac0ae509fc185142a0
Charles Fettinger <charles@onacloud.org>
2023-03-21 08:35:16 -0700

    update requirements

--- webui-user.bat
+++ webui-user.bat
@@ -4,8 +4,11 @@ set PYTHON=C:\Users\CharlesFettinger\AppData\Local\Programs\Python\Python310\pyt
 set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=--gfpgan-model GFPGANv1.4.pth --xformers --no-progressbar-hiding --gradio-img2img-tool color-sketch --skip-torch-cuda-test --disable-safe-unpickle
+set "XFORMERS_PACKAGE=xformers==0.0.17.dev447"
 rem --xformers --precision full --no-half  --medvram
 set 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:128'
 rem set 'OC_CAUSE=1'
 rem set ACCELERATE="True"
+set "REQS_FILE=.\extensions\sd_dreambooth_extension\requirements.txt"
+
 call webui.bat
